75d84e8a6dcd25033b9c0c9dad8e999d145e9d04
AdrianaPiracoca <lapiracoca@jdc.edu.co>
2023-11-14 11:39:17 -0500

    Se agregó UpdateOwnerDTOs

---
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/owner/dto/UpdateOwnerReqDTO.java
@@ -0,0 +1,21 @@
+package us.codezen.owner.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@AllArgsConstructor
+@Getter
+@Setter
+public class UpdateOwnerReqDTO{
+    private int identification;
+    private String firstName;
+    private String secondName;
+    private String firstLastName;
+    private String SecondLastName;
+    private Date birthday;
+    private int phone;
+    private String email;
+    private String typeIdentification;
+}
