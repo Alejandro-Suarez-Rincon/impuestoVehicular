55943c0f463a7c0dbb9493e6b6fad166075c62e4
AdrianaPiracoca <lapiracoca@jdc.edu.co>
2023-11-14 11:37:02 -0500

    Se agregó CreateOwnerDTOs

---
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/owner/dto/CreateOwnerReqDTO.java
@@ -0,0 +1,20 @@
+package us.codezen.owner.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+@AllArgsConstructor
+@Getter
+@Setter
+public class CreateOwnerReqDTO{
+        private int identification;
+        private String firstName;
+        private String secondName;
+        private String firstLastName;
+        private String SecondLastName;
+        private Date birthday;
+        private int phone;
+        private String email;
+        private String typeIdentification;
+}
---
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/owner/dto/CreateOwnerResDTO.java
@@ -0,0 +1,13 @@
+package us.codezen.owner.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@AllArgsConstructor
+@Getter
+@Setter
+public class CreateOwnerResDTO {
+    private String message;
+}
