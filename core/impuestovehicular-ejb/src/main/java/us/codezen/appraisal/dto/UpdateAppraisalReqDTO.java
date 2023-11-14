a3fe903806d627c1cf5901350ff3598d18b3e256
faridcaro666 <110434449+faridcaro666@users.noreply.github.com>
2023-11-14 11:48:33 -0500

    Se realisa UpdateAppraisalResDTO

---
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/appraisal/dto/UpdateAppraisalReqDTO.java
@@ -0,0 +1,26 @@
+import lombok.Data;
+@Data
+public class UpdateAppraisalReqDTO{
+    Private long id ;
+    Private String tipo;
+    Private String clase;
+    Private String marca;
+    Private String linea;
+    Private String tonelaje;
+    Private String pasajeros;
+    Private String carroceria;
+    Private String cilindraje;
+    Private String valorAntesIva;
+    Private String modelo;
+    Private boolean importado;
+    Private boolean blindado;
+    Private boolean cajaAutomatica;
+    Private boolean cartasAbiertas;
+    Private boolean decomisado;
+    Private boolean chatarrizado;
+    Private String estado;
+    Private String extecionDominio;
+    Private boolean antiguo;
+        
+       }
+     
---
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/appraisal/dto/UpdateAppraisalResDTO.java
@@ -0,0 +1,15 @@
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class UpdateAppraisalResDTO{
+     Private long id ;
+    Private String tipo;
+    Private String clase;
+    Private String marca;
+    Private String linea;
+    Private String tonelaje;
+    Private String pasajeros;
+    Private String carroceria;
+    Private String cilindraje;
+    Private String valorAntesIva;
+      }
