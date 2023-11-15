a54f47396db9877fe3bf7edfc357f4241bc8bcaf
Estefania-Araque <lauraestefaniaaraque@gmail.com>
2023-11-15 18:27:20 -0500

    Se cambiaron los datos de salida y se elimino el comentario

--- core/impuestovehicular-ejb/src/main/java/us/codezen/vehicle/dto/UpdateVehicleResDTO.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/vehicle/dto/UpdateVehicleResDTO.java
@@ -1,6 +1,7 @@
 import lombok.Data;
 @Data
 @AllArgsConstructor
-public class UpdateVehicleReSDTO(
-    Private String mensaje;  //"El vehiculo se ha actualizado"
+public class CreateVehicleResqDTO(
+    private String idOwner;
+    private String idVehicle;
 )
