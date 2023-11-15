d83853e15cc28c8fb9709578fb8cdb099d4c61c8
faridcaro666 <110434449+faridcaro666@users.noreply.github.com>
2023-11-15 18:23:18 -0500

    se arreglo actualizar avaluo
    se pasaron ingles y y se dejaron los datos tal como están  en la base de datos

--- core/impuestovehicular-ejb/src/main/java/us/codezen/appraisal/dto/UpdateAppraisalReqDTO.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/appraisal/dto/UpdateAppraisalReqDTO.java
@@ -2,25 +2,25 @@ import lombok.Data;
 @Data
 public class UpdateAppraisalReqDTO{
     Private long id ;
-    Private String tipo;
-    Private String clase;
-    Private String marca;
-    Private String linea;
-    Private String tonelaje;
-    Private String pasajeros;
-    Private String carroceria;
-    Private String cilindraje;
-    Private String valorAntesIva;
-    Private String modelo;
-    Private boolean importado;
-    Private boolean blindado;
-    Private boolean cajaAutomatica;
-    Private boolean cartasAbiertas;
-    Private boolean decomisado;
-    Private boolean chatarrizado;
-    Private String estado;
-    Private String extecionDominio;
-    Private boolean antiguo;
+    Private String type;
+    Private String class;
+    Private String flag;
+    Private String line;
+    Private String tonnage;
+    Private String passengers;
+    Private String bodywork;
+    Private String displacement;
+    Private String valueBeforeIva;
+    private String model;
+    private boolean imported;
+    private boolean shielded;
+    private boolean automaticBox;
+    private boolean openCards;
+    private boolean confiscated;
+    private boolean scrapped;
+    private String state;
+    Private String domainextecion;
+    private boolean old;
         
        }
      
--- core/impuestovehicular-ejb/src/main/java/us/codezen/appraisal/dto/UpdateAppraisalResDTO.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/appraisal/dto/UpdateAppraisalResDTO.java
@@ -2,14 +2,14 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class UpdateAppraisalResDTO{
-     Private long id ;
-    Private String tipo;
-    Private String clase;
-    Private String marca;
-    Private String linea;
-    Private String tonelaje;
-    Private String pasajeros;
-    Private String carroceria;
-    Private String cilindraje;
-    Private String valorAntesIva;
+    private long id ;
+    private String type;
+    private String class;
+    Private String flag;
+    Private String line;
+    Private String tonnage;
+    Private String passengers;
+    Private String bodywork;
+    Private String displacement;
+    Private String valueBeforeIva;
       }
