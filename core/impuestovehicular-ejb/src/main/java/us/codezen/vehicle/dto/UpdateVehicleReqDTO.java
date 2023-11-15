631f15a0655f16112c6aed9c0109641d37accb97
Estefania-Araque <lauraestefaniaaraque@gmail.com>
2023-11-14 22:27:02 -0500

    Crear entradas y salidas (Actualizar vehículo)

---
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/vehicle/dto/UpdateVehicleReqDTO.java
@@ -0,0 +1,27 @@
+import lombok.Data;
+@Data
+public class UpdateVehicleReqDTO(
+    Private String placa;
+    Private String tipo_de_sevicio;
+    Private String clase;
+    Private String marca_comercial;
+    Private String linea;
+    Private String tonelaje;
+    Private String pasajeros;
+    Private String carroceria;
+    Private String capacidad_de_cilindraje;
+    Private String valor_de_IVA;
+    Private String modelo;
+    Private boolean importado;
+    Private boolean blindado;
+    Private boolean caja_automatica;
+    Private boolean cartas_abiertas;
+    Private boolean rodado;
+    Private boolean chatarrizado;
+    Private boolean Confiscado;
+    Private boolean Accidentado;
+    Private String estado;
+    Private String dominio_de_extencion;
+    Private String tipo_de_vehiculo;
+    Private String antiguo;
+)
---
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/vehicle/dto/UpdateVehicleResDTO.java
@@ -0,0 +1,6 @@
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class UpdateVehicleReSDTO(
+    Private String mensaje;  //"El vehiculo se ha actualizado"
+)
