e0fb347b4c65b879803f044cfc9b13aa8e37848f
Estefania-Araque <lauraestefaniaaraque@gmail.com>
2023-11-15 18:26:24 -0500

    Se realizaron las entradas y salidas del buscar vehículo

---
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/vehicle/dto/SearchVehicleResDTO.java
@@ -0,0 +1,27 @@
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class CreateVehicleResDTO(
+    private String plate;
+    private String typeService;
+    private String idCharacteristicsVehicle;
+    private String trademark
+    private String line;
+    private String valueIva;
+    private boolean ancient;
+    private String model;
+    private String bodywork;
+    private String cylinderCapacity;
+    private String tonnage;
+    private String passengers;
+    private boolean imported;
+    private boolean blindado;
+    private boolean armored;
+    private boolean automaticBox;
+    private boolean openLetters;
+    private boolean stolen;
+    private boolean confiscated;
+    private boolean scrapped;
+    private String state;
+    private String domainExtension;
+)
