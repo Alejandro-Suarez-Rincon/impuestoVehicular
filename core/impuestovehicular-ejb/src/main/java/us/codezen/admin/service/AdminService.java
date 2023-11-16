ad8aadd4550e525131c1f905f47855b2c1deef53
camilo-camargo <camilocamargo49@gmail.com>
2023-11-16 01:05:30 -0500

    Se elimino admin repository.

--- core/impuestovehicular-ejb/src/main/java/us/codezen/admin/service/AdminService.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/admin/service/AdminService.java
@@ -2,21 +2,22 @@ package us.codezen.admin.service;
 
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import us.codezen.admin.dto.CreateAdminReqDTO;
 import us.codezen.admin.dto.CreateAdminResDTO;
 import us.codezen.admin.entity.Admin;
-import us.codezen.admin.repository.AdminRepository;
 
 @Stateless
 public class AdminService {
-  @Inject
-  private AdminRepository adminRepository;
+  @PersistenceContext
+  private EntityManager em;
 
   public CreateAdminResDTO createAdmin(CreateAdminReqDTO createAdminReqDTO) {
     final Admin admin = new Admin(
         createAdminReqDTO.getEmail(),
         createAdminReqDTO.getPassword());
-    adminRepository.createAdmin(admin);
+    em.persist(admin);
 
     return new CreateAdminResDTO(admin.getId(), admin.getEmail());
   }
