46dbecf841f3e0898f607e6dc0da5fdeda8dc49f
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2023-11-15 22:26:53 -0500

    cambios

--- core/impuestovehicular-ejb/src/main/java/us/codezen/report/dto/CalculateReportResDTO.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/report/dto/CalculateReportResDTO.java
@@ -1,11 +1,9 @@
 package us.codezen.report.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@NoArgsConstructor
 @Data
 public class CalculateReportResDTO {
     private List listPaidValidity;
--- core/impuestovehicular-ejb/src/main/java/us/codezen/report/dto/GenerateReportResDTO.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/report/dto/GenerateReportResDTO.java
@@ -1,9 +1,8 @@
 package us.codezen.report.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+
 @Data
 public class GenerateReportResDTO {
     private String path;
