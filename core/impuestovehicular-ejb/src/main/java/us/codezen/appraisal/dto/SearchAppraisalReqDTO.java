7775802d70102d846a531515862fd8c41af102a9
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2023-11-18 22:13:03 -0500

    Correccion en DTO avaluoREQ

--- core/impuestovehicular-ejb/src/main/java/us/codezen/appraisal/dto/SearchAppraisalReqDTO.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/appraisal/dto/SearchAppraisalReqDTO.java
@@ -1,10 +1,50 @@
 package us.codezen.appraisal.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class SearchAppraisalReqDTO {
-    @NotNull
+    @NonNull
     private Long id;
+    @NonNull
+    private String type;
+    @NonNull
+    private String classVehicle;
+    @NonNull
+    private String trademark;
+    @NonNull
+    private String line;
+    @NonNull
+    private String tonnage;
+    @NonNull
+    private String bodywork;
+    @NonNull
+    private String displacement;
+    @NonNull
+    private String vatValue;
+    @NonNull
+    private String model;
+    @NonNull
+    private boolean imported;
+    @NonNull
+    private boolean shielded;
+    @NonNull
+    private boolean automaticBox;
+    @NonNull
+    private boolean openLetters;
+    @NonNull
+    private boolean scrapped;
+    @NonNull
+    private String state;
+    @NonNull
+    private String domainextension;
+    @NonNull
+    private boolean old;
+    @NonNull
+    private String vehicleType;
+    @NonNull
+    private String passengers;
+    @NonNull
+    private String stolen;
 }
