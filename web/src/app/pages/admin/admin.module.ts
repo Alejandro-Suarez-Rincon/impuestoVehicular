3ff42f52d328e25f5394b37573a4540b148086b6
LDRubiano <laurarubiano005@gmail.com>
2023-11-20 12:46:25 -0500

    Avaluos html

--- web/src/app/pages/admin/admin.module.ts
+++ web/src/app/pages/admin/admin.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppraisalsComponent } from './appraisals/appraisals.component';
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    AppraisalsComponent
   ],
   imports: [
     CommonModule,
