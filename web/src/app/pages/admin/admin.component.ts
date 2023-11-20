a96dbdfde58af6422a1d7026bbb47140520e5fef
Santiago-Cujaban <79294248+santiago-cujaban@users.noreply.github.com>
2023-11-19 22:56:19 -0500

    Funcionalidad pantalla login
    :cat:  Ahora en la pantalla login se puede enviar el json con la informacion correspondiente, siempre y cuando se seleccione el botón de terminos y condiciones. :cat:

--- web/src/app/pages/admin/admin.component.ts
+++ web/src/app/pages/admin/admin.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
+  standalone: false,
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+
+  terms: boolean = false;
+
+  form = new FormGroup({
+    correo_electronico: new FormControl(''),
+    contrasena: new FormControl('')
+  })
+
+  // Función que envia el json al servicio REST
+  handleSubmit(){
+    console.log(this.form.value)
+  }
+
+  // Función que redirecciona a la pantalla de login
   goToLogin(){
     window.location.href="/admin"
+  }
+
+  // Función que modifica el estado del checkbox
+  toggleTerms(){
+    this.terms= !this.terms
   }
 }
--- web/src/app/pages/admin/admin.module.ts
+++ web/src/app/pages/admin/admin.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { AdminComponent } from './admin.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
