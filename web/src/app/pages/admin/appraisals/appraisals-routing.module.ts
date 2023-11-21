bc7fc3902f2564d4f1e122ef1a860657096e2832
LDRubiano <laurarubiano005@gmail.com>
2023-11-21 00:42:53 -0500

    se arregla las clases de moduloa y rutas para avaluos

---
+++ web/src/app/pages/admin/appraisals/appraisals-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppraisalsComponent } from './appraisals.component';
+
+const routes: Routes = [{ path: '', component: AppraisalsComponent}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AppraisalsRoutingModule { }
