546e8de85a4bf14c2624faa6897bbb691959ab85
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2023-11-20 22:17:27 -0500

    Generar Pdf priera parte

--- core/impuestovehicular-ejb/src/main/java/us/codezen/report/service/ReportService.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/report/service/ReportService.java
@@ -1,9 +1,20 @@
 package us.codezen.report.service;
 
+import com.itextpdf.text.DocumentException;
+import jakarta.persistence.EntityManager;
 import us.codezen.appraisal.dto.QueryAppraisalResDTO;
+import us.codezen.appraisal.entities.Appraisal;
 import us.codezen.report.dto.CalculateReportResDTO;
+import us.codezen.report.dto.GenerateReportResDTO;
 import us.codezen.vehicle.dto.SearchVehicleResDTO;
 import us.codezen.vehicle.enums.VehicleTypeServiceEnum;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 
 public class ReportService {
     public CalculateReportResDTO query(CalculateReportResDTO calculateReportResDTO) {
@@ -34,8 +45,10 @@ public class ReportService {
                 "CAMIONETAS Y CAMPEROS"
         );
 
-        // Duda en estos datos
+        String id = vehicle.getIdCharacteristicsVehicle();
+        String type = vehicle.getType();
 
+        // Duda en estos datos
         // QueryAppraisalResDTO appraisal = appraisalService(new QueryAppraisalResDTO(query(query(queryQueryApraisal.id))));
         QueryAppraisalResDTO appraisal = new QueryAppraisalResDTO(
                 1L,
@@ -61,7 +74,34 @@ public class ReportService {
                 "NO"
         );
 
+        // Traer el valor de appraisal
+        //Double value = appraisal.getValue();
+
+
 
         return calculateReportResDTO;
+    }
+
+    public GenerateReportResDTO query(GenerateReportResDTO generateReportResDTO){
+        Document document = new Document();
+
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("Reporte"));
+
+
+
+        } catch (DocumentException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        document.open();
+        // Aqui tiene que traer y guardar todo en el pdf
+
+
+        document.close();
+
+        return generateReportResDTO;
     }
 }
