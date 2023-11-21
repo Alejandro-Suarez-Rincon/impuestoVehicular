14ece4c9975939dc9ce48f26378ca11d32eccba2
Dnvla <143069256+Dnvla@users.noreply.github.com>
2023-11-21 02:38:20 -0500

    Update Owner.java

--- core/impuestovehicular-ejb/src/main/java/us/codezen/owner/entities/Owner.java
+++ core/impuestovehicular-ejb/src/main/java/us/codezen/owner/entities/Owner.java
@@ -13,9 +13,7 @@ import java.time.LocalDateTime;
 @Data
 public class Owner {
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    private Long id_owner;
-
+    @GeneratedValue(strategy =  GenerationType.AUTO)
     @Column(name = "identification", nullable = false)
     private  Long identification;
 
