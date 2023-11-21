e6b010a03fe1b01e9161fa549461c66771ad158c
camilo-camargo <camilocamargo49@gmail.com>
2023-11-21 05:09:59 -0500

    Se agrego de login admin

--- web/src/app/pages/admin/admin.component.ts
+++ web/src/app/pages/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CoreAPIService } from 'src/app/services/core-api.service';
 
 @Component({
   selector: 'app-admin',
@@ -9,10 +10,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-  constructor(private router:Router){}
+  constructor(private router: Router,
+    private readonly coreAPIService: CoreAPIService) { }
+
+  isLoggedIn: boolean = false;
 
   // Estado checkbox terminos y condiciones
   terms: boolean = false;
+
 
   // Información Formulario
   form = new FormGroup({
@@ -21,17 +26,29 @@ export class AdminComponent {
   })
 
   // Función que envia el json al servicio REST
-  handleSubmit(){
-    console.log(this.form.value)
+  handleSubmit() {
+    this.coreAPIService.post({
+      path: "/admin/validate",
+      body: this.form.value,
+    }).subscribe(data => {
+      const adminValidateResDTO: {
+        id: string;
+        email: string;
+      } = data as any;
+
+      if (adminValidateResDTO.id) {
+        this.router.navigate(['/admin/create-owner']);
+      }
+    });
   }
 
   // Función que redirecciona a la pantalla de login
-  goToLogin(){
+  goToLogin() {
     this.router.navigate(['/admin'])
   }
 
   // Función que modifica el estado del checkbox
-  toggleTerms(){
-    this.terms= !this.terms
+  toggleTerms() {
+    this.terms = !this.terms
   }
 }
